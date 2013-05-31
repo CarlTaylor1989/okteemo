e60ac0bd5164fea6d22faf87e3174277c46bc33f
carl taylor <carl.t@dnxmarketing.com>
2013-05-31 14:34:39 +0100

    More CSS updates and added feedback feature

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -5,6 +5,7 @@
 				<div class="well main-desc">
 					<h1>OKteemo <small>Get yours teams here.</small></h1>
 					<p>Info here talking about what this site offers etc.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 				</div>
 			</div>
 			<div class="span4 reg_form">
@@ -49,7 +50,7 @@
 	</div>
 </div>
 <div class="container">
-	<div class="row">
+	<div class="row col-3">
 		<div class="span4">
 			<div class="looking-team">
 				<h4>I'm looking for a<br /><span>TEAM</span></h4>
@@ -80,7 +81,61 @@
 	</div>
 	<div class="row">
 		<div class="span12">
-			
+			<div id="myCarousel" class="carousel slide">
+			  <ol class="carousel-indicators">
+			    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			    <li data-target="#myCarousel" data-slide-to="1"></li>
+			    <li data-target="#myCarousel" data-slide-to="2"></li>
+			  </ol>
+			  <!-- Carousel items -->
+			  <div class="carousel-inner">
+			    <div class="active item">
+			    	<div class="span4 no-marg">
+			    		<p class="feedback-comment">"Found a 5v5 within 20 minutes and managed to achieve platinum! Thank you Okteemo."</p>
+			    		<p class="feedback-author">CarlTaylor1989 - EUW</p>
+			    	</div>
+			    	<div class="span4">
+			    		<p class="feedback-comment">"Found a 5v5 within 20 minutes and managed to achieve platinum! Thank you Okteemo."</p>
+			    		<p class="feedback-author">IRazorX - US</p>
+			    	</div>
+			    	<div class="span4">
+			    		<p class="feedback-comment">"Found a 5v5 within 20 minutes and managed to achieve platinum! Thank you Okteemo."</p>
+			    		<p class="feedback-author">Touch_Me - EUW</p>
+			    	</div>
+			    </div>
+			    <div class="item">
+			    	<div class="span4 no-marg">
+			    		<p class="feedback-comment">"Found a 5v5 within 20 minutes and managed to achieve platinum! Thank you Okteemo."</p>
+			    		<p class="feedback-author">CarlTaylor1989 - EUW</p>
+			    	</div>
+			    	<div class="span4">
+			    		<p class="feedback-comment">"Found a 5v5 within 20 minutes and managed to achieve platinum! Thank you Okteemo."</p>
+			    		<p class="feedback-author">IRazorX - US</p>
+			    	</div>
+			    	<div class="span4">
+			    		<p class="feedback-comment">"Found a 5v5 within 20 minutes and managed to achieve platinum! Thank you Okteemo."</p>
+			    		<p class="feedback-author">Touch_Me - EUW</p>
+			    	</div>
+			    </div>
+			    <div class="item">
+			    	<div class="span4 no-marg">
+			    		<p class="feedback-comment">"Found a 5v5 within 20 minutes and managed to achieve platinum! Thank you Okteemo."</p>
+			    		<p class="feedback-author">CarlTaylor1989 - EUW</p>
+			    	</div>
+			    	<div class="span4">
+			    		<p class="feedback-comment">"Found a 5v5 within 20 minutes and managed to achieve platinum! Thank you Okteemo."</p>
+			    		<p class="feedback-author">IRazorX - US</p>
+			    	</div>
+			    	<div class="span4">
+			    		<p class="feedback-comment">"Found a 5v5 within 20 minutes and managed to achieve platinum! Thank you Okteemo."</p>
+			    		<p class="feedback-author">Touch_Me - EUW</p>
+			    	</div>
+			    </div>
+			  </div>
+			  <!-- Carousel nav -->
+			  <!-- <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+			  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a> -->
+			</div>
 		</div>
 	</div>
 </div>
