d2d8e9408d1e91b787c1fd7b9a6beecd24fc4526
carl taylor <carl.t@dnxmarketing.com>
2013-05-31 12:43:54 +0100

    Reg form CSS changes, background added

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,48 +1,54 @@
-<div class="container">
-	<div class="row">
-		<div class="span8">
-			<h1>OKteemo <small>Get yours teams here.</small></h1>
-			<p>Info here talking about what this site offers etc.</p>
-		</div>
-		<div class="span4">
-			<div class="well well-small">
-				<h3>Register to get started!</h3>
-				<form>
-					<div class="control-group">
-						<label class="control-label" for="reg_username">Username</label>
-						<div class="controls">
-					    	<div class="input-prepend">
-					      		<span class="add-on"><i class="icon-user"></i></span>
-					      		<input class="span3" id="reg_username" name="reg_username" type="text" placeholder="Username">
-					    	</div>
-					  	</div>
-						<label class="control-label" for="reg_email">Email address</label>
-						<div class="controls">
-					    	<div class="input-prepend">
-					      		<span class="add-on"><i class="icon-envelope"></i></span>
-					      		<input class="span3" id="reg_email" name="reg_email" type="text" placeholder="Email address">
-					    	</div>
-					  	</div>
-					  	<label class="control-label" for="reg_password">Password</label>
-						<div class="controls">
-					    	<div class="input-prepend">
-					      		<span class="add-on"><i class="icon-lock"></i></span>
-					      		<input class="span3" id="reg_password" name="reg_password" type="text" placeholder="Password">
-					    	</div>
-					  	</div>
-					  	<label class="control-label" for="reg_conf_password">Confirm Password</label>
-						<div class="controls">
-					    	<div class="input-prepend">
-					      		<span class="add-on"><i class="icon-lock"></i></span>
-					      		<input class="span3" id="reg_conf_password" name="reg_conf_password" type="text" placeholder="Confirm Password">
-					    	</div>
-					  	</div>
-					  	<button class="btn btn-large btn-danger">Let's get started!</button>
-					</div>
-				</form>
+<div class="bg">
+	<div class="container">
+		<div class="row">
+			<div class="span8">
+				<div class="well main-desc">
+					<h1>OKteemo <small>Get yours teams here.</small></h1>
+					<p>Info here talking about what this site offers etc.</p>
+				</div>
+			</div>
+			<div class="span4 reg_form">
+				<div class="well">
+					<h3>Register for free!</h3>
+					<form>
+						<div class="control-group">
+							<label class="control-label" for="reg_username">Username</label>
+							<div class="controls">
+						    	<div class="input-prepend">
+						      		<span class="add-on"><i class="icon-user"></i></span>
+						      		<input class="span3" id="reg_username" name="reg_username" type="text" placeholder="Username" />
+						    	</div>
+						  	</div>
+							<label class="control-label" for="reg_email">Email address</label>
+							<div class="controls">
+						    	<div class="input-prepend">
+						      		<span class="add-on"><i class="icon-envelope"></i></span>
+						      		<input class="span3" id="reg_email" name="reg_email" type="text" placeholder="Email address" />
+						    	</div>
+						  	</div>
+						  	<label class="control-label" for="reg_password">Password</label>
+							<div class="controls">
+						    	<div class="input-prepend">
+						      		<span class="add-on"><i class="icon-lock"></i></span>
+						      		<input class="span3" id="reg_password" name="reg_password" type="text" placeholder="Password" />
+						    	</div>
+						  	</div>
+						  	<label class="control-label" for="reg_conf_password">Confirm Password</label>
+							<div class="controls">
+						    	<div class="input-prepend">
+						      		<span class="add-on"><i class="icon-lock"></i></span>
+						      		<input class="span3" id="reg_conf_password" name="reg_conf_password" type="text" placeholder="Confirm Password" />
+						    	</div>
+						  	</div>
+						  	<input type="submit" class="btn btn-large btn-danger" value="Let's get started!" />
+						</div>
+					</form>
+				</div>
 			</div>
 		</div>
 	</div>
+</div>
+<div class="container">
 	<div class="row">
 		<div class="span4">
 			<div class="looking-team">
